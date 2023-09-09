170f06fa30e78dce68f6e78deb403ac84443211a
Victor <vmontoli@student.42barcelona.com>
2023-09-09 06:22:24 +0200

    Copiado de mandatory a bonus, falta incluir la funcionalidad bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: vmontoli <vmontoli@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 08:28:41 by vmontoli          #+#    #+#             */
-/*   Updated: 2023/08/18 08:29:33 by vmontoli         ###   ########.fr       */
+/*   Updated: 2023/09/09 06:18:25 by vmontoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,90 @@
 
 char	*get_next_line(int fd)
 {
+	static t_buffer_node	*buffer_list = NULL;
+	t_buffer_node			*curr_buff_node;
+	char					*result;
+
+	if (fd < 0 || read(fd, NULL, 0) == -1)
+		return (free_buffer_list(&buffer_list, false));
+	if (buffer_list == NULL)
+		new_buffer_node(fd, &buffer_list);
+	curr_buff_node = buffer_list;
+	while (curr_buff_node != NULL)
+	{
+		set_newline_pos(curr_buff_node);
+		if (curr_buff_node->has_newline || curr_buff_node->has_eof)
+		{
+			result = generate_result(buffer_list);
+			if (result == NULL)
+				return (free_buffer_list(&buffer_list, false));
+			tidy_buff_list(&buffer_list);
+			return (result);
+		}
+		new_buffer_node(fd, &curr_buff_node);
+	}
+	return (free_buffer_list(&buffer_list, false));
+}
+
+void	set_newline_pos(t_buffer_node *buff_node)
+{
+	size_t	i;
+
+	i = 0;
+	buff_node->has_newline = false;
+	while (i < buff_node->size)
+	{
+		if (buff_node->start[i] == '\n')
+		{
+			buff_node->has_newline = true;
+			buff_node->newline_pos = i;
+			break ;
+		}
+		i++;
+	}
+}
+
+void	tidy_buff_list(t_buffer_node **buffer_list_ptr)
+{
+	free_buffer_list(buffer_list_ptr, true);
+	if ((*buffer_list_ptr)->has_newline
+		&& ((*buffer_list_ptr)->size > (*buffer_list_ptr)->newline_pos + 1))
+	{
+		(*buffer_list_ptr)->start += (*buffer_list_ptr)->newline_pos + 1;
+		(*buffer_list_ptr)->size -= (*buffer_list_ptr)->newline_pos + 1;
+	}
+	else
+	{
+		free(*buffer_list_ptr);
+		*buffer_list_ptr = NULL;
+	}
+}
+
+char	*generate_result(t_buffer_node *buffer_list)
+{
+	size_t			i;
+	t_buffer_node	*curr_buff_node;
+	char			*result;
+	char			*curr_result;
+
+	i = 1;
+	curr_buff_node = buffer_list;
+	while (curr_buff_node != NULL)
+	{
+		i += copy_buff_node_to_str(curr_buff_node, NULL);
+		curr_buff_node = curr_buff_node->next;
+	}
+	result = (char *) malloc(i * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	curr_result = result;
+	curr_buff_node = buffer_list;
+	while (curr_buff_node != NULL)
+	{
+		i = copy_buff_node_to_str(curr_buff_node, curr_result);
+		curr_result += i;
+		curr_buff_node = curr_buff_node->next;
+	}
+	*curr_result = '\0';
+	return (result);
 }
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,8 +6,84 @@
 /*   By: vmontoli <vmontoli@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 08:31:08 by vmontoli          #+#    #+#             */
-/*   Updated: 2023/08/18 08:31:23 by vmontoli         ###   ########.fr       */
+/*   Updated: 2023/09/09 06:19:16 by vmontoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
+
+void	new_buffer_node(int fd, t_buffer_node **prev_ptr)
+{
+	t_buffer_node	*buff_node;
+	ssize_t			bytes_read;
+
+	buff_node = malloc(sizeof(t_buffer_node));
+	if (buff_node == NULL)
+		return ;
+	bytes_read = read(fd, buff_node->buffer, BUFFER_SIZE);
+	if (bytes_read == -1 || bytes_read == 0)
+	{
+		if (bytes_read == 0 && *prev_ptr != NULL)
+			(*prev_ptr)->has_eof = true;
+		else
+			*prev_ptr = NULL;
+		free(buff_node);
+		return ;
+	}
+	if (*prev_ptr != NULL)
+		(*prev_ptr)->next = buff_node;
+	buff_node->start = buff_node->buffer;
+	buff_node->has_eof = bytes_read < BUFFER_SIZE;
+	buff_node->size = (size_t) bytes_read;
+	buff_node->next = NULL;
+	*prev_ptr = buff_node;
+}
+
+void	*free_buffer_list(t_buffer_node **buffer_list_ptr,
+					bool maintain_last)
+{
+	t_buffer_node	*aux_curr;
+	t_buffer_node	*aux_next;
+
+	if (buffer_list_ptr == NULL)
+		return (NULL);
+	aux_curr = *buffer_list_ptr;
+	if (aux_curr == NULL)
+		return (NULL);
+	aux_next = aux_curr->next;
+	while (aux_next != NULL)
+	{
+		free(aux_curr);
+		aux_curr = aux_next;
+		aux_next = aux_curr->next;
+	}
+	if (maintain_last)
+		*buffer_list_ptr = aux_curr;
+	else
+	{
+		free(aux_curr);
+		*buffer_list_ptr = NULL;
+	}
+	return (*buffer_list_ptr);
+}
+
+size_t	copy_buff_node_to_str(t_buffer_node *curr_buff_node, char *str)
+{
+	size_t	i;
+	size_t	len_to_copy;
+
+	if (curr_buff_node->has_newline)
+		len_to_copy = curr_buff_node->newline_pos + 1;
+	else
+		len_to_copy = curr_buff_node->size;
+	if (str != NULL)
+	{
+		i = 0;
+		while (i < len_to_copy)
+		{
+			str[i] = curr_buff_node->start[i];
+			i++;
+		}
+	}
+	return (len_to_copy);
+}
